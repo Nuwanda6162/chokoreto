edf626dd51900d8c85fb2874e5962693877e1e92
Nuwanda6162 <javyandc@gmail.com>
2025-06-24 13:57:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ import numpy as np
 import os
 import psycopg2
 
-# Conectar a la base SQLite local
-#import sqlite3
-#conn = sqlite3.connect("chokoreto_costos.db", check_same_thread=False)
-#cursor = conn.cursor()
-
-# Conectar a la base Supabase
-
 #load_dotenv()  # Esto carga el .env
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == APP_PASSWORD:
+            st.session_state["password_correct"] = True
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+        st.stop()
+    elif not st.session_state["password_correct"]:
+        st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+        st.error("Contraseña incorrecta")
+        st.stop()
+
+check_password()
 
 conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
